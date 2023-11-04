e70ed5dcbc23c7675797a8852ad60b65f3534e9f
lhjun0121 <lhjun0121@naver.com>
2023-11-04 19:11:55 +0900

    Find the Runner-Up Score! solution commit

---
+++ lhjun0121/Find_the_Runner-Up_Score!.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    mx = max(arr)
+    arrset = set(arr)
+    arrset.remove(mx)
+    print(max(arrset))
+
