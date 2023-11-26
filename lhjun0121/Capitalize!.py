731e10656b31f28dce9f118f6b78280eaeb62448
lhjun0121 <lhjun0121@naver.com>
2023-11-26 18:52:46 +0900

    Capitalize! solution fix

--- lhjun0121/Capitalize!.py
+++ lhjun0121/Capitalize!.py
@@ -8,7 +8,7 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
-    return " ".join([i.capitalize() for i in s.split()])
+    return " ".join([i.capitalize() for i in s.split(" ")])
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
