c2f6b5b9da0f2249e4d3887e9e9a34555cfde310
Ash <ashkim2001@gmail.com>
2024-01-10 11:45:38 +0900

    Matrix Script solution commit

---
+++ ashz/Matrix_Script.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+p = r'(?<=\w)\W+(?=\w)'
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+text = ''
+for i in range(m):
+    for j in range(n):
+        text += matrix[j][i]
+
+res = re.sub(p, ' ', text)
+print(res)
