526c47b189b83031c9b653839d9b01e2f52ae43a
Ash <ashkim2001@gmail.com>
2024-02-01 21:17:21 +0900

    Check Subset solution commit

---
+++ ashz/Check_Subset.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+
+for _ in range(T):
+    Ac = int(input())
+    A = input().split(' ')
+    Bc = int(input())
+    B = input().split(' ')
+    check = True
+    for i in A:
+        if not (i in B):
+            check = False
+            break
+    if check:
+        print('True')
+    else:
+        print('False'
