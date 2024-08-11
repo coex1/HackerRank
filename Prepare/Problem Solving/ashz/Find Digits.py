630d409b150bcfb6fa34bda2a7f1fb15d1642154
Ash <ashkim2001@gmail.com>
2024-08-11 21:32:23 +0900

    Find Digits solution commit

---
+++ Prepare/Problem Solving/ashz/Find Digits.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    c = 0
+    sn = str(n)
+    for i in sn:
+        if i == '0':
+            continue
+        if n % int(i) == 0:
+            c += 1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
