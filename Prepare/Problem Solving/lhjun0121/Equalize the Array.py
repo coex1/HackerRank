dbfe6c76e8a18b9d463220edd30984ec7c0b8e22
lhjun0121 <103979565+lhjun0121@users.noreply.github.com>
2024-09-23 21:40:19 +0900

    Equalize the Array solution commit

---
+++ Prepare/Problem Solving/lhjun0121/Equalize the Array.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    frq = max(arr, key = arr.count)
+    res = len(arr) - arr.count(frq)
+    return res
+
+if __name__ == '__main__':    
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open('OUTPUT_PATH', 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
