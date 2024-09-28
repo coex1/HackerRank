24494b5e944e0ed364bbb0977cbcc335e0d8aa21
Ash <ashkim2001@gmail.com>
2024-09-29 07:20:11 +0900

    Equalize the Array solution commit

---
+++ Prepare/Problem Solving/ashz/Equalize the Array.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    import collections
+    c = collections.Counter(arr)
+    high = -1
+    t = 0
+    for k, v in c.items():
+        if v > high:
+            high = v
+            t = k
+    
+    return len(arr) - high
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
